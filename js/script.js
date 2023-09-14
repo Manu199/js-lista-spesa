03626b8c68c6e222066ba4029202294080f93828
Manu199 <manu199gh@gmail.com>
2023-09-14 14:28:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+
+creare una lista della spesa della.
+stampare la lista della spesa sul html con un ciclo while.
+
+ */
+
+const lista = ('mele', 'pere', 'melanzane', 'latte', 'sugo', 'pasta')
