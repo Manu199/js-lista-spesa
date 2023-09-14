dacfd27a1a99e995f0393719e126177c39fbd5ae
Manu199 <manu199gh@gmail.com>
2023-09-14 14:44:04 +0200

    [add] while

--- js/script.js
+++ js/script.js
@@ -5,4 +5,21 @@ stampare la lista della spesa sul html con un ciclo while.
 
  */
 
-const lista = ('mele', 'pere', 'melanzane', 'latte', 'sugo', 'pasta')
+const lista = ['mele', 'pere', 'melanzane', 'latte', 'sugo', 'pasta'];
+
+let i = 0;
+
+
+while (i < lista.length) {
+  console.log(lista[i])
+  i++;
+  
+}
+
+
+  document.getElementById('list').innerHTML = lista + ' ' + i;
+
+
+
+
+
